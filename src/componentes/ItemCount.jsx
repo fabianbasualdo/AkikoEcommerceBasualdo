451d0f7609768e1e3c65d5b4fe78c237fb5d4668
Marcelo Fabian Basualdo <fabianbausaldo0909@gmail.com>
2022-09-15 00:22:31 -0300

    explicacion

--- src/componentes/ItemCount.jsx
+++ src/componentes/ItemCount.jsx
@@ -2,17 +2,18 @@
 
 import { useEffect, useState } from 'react';
 
-
+/*al parametro "initial" llegara el useState llamado itemCount, 
+que se encuentra declarado en el componente ItemDetail */
 const ItemCount = ({ stock = 0, initial = 1,  onAdd }) => {
     const [count, setCount] = useState(0);
 
     useEffect(() => {
-        setCount(initial);
+        setCount(initial); /*con el valor de initial seteo el useState llamado count*/
     },[initial]);
 
     const increment = () => {
         if (count < stock) {
-            setCount(count + 1);
+            setCount(count + 1);/*cada vez que cambie el valor de count cambiara el useState itemCount, porque entro a traves del parametro llamado initial*/
         }
     }
     
@@ -23,12 +24,17 @@ const ItemCount = ({ stock = 0, initial = 1,  onAdd }) => {
     }
     return (
         <>
+
+        {/*al precionar el boton va cambiando el valor del useState count */}
             <button variant="text" onClick={increment} >+</button>
            <span>{count}</span>
             <button variant="text" onClick={decrement}>-</button>
             {
+                /*en la primera carga del DOM, existe stock, pero "count" es cero
+                es por eso que cargara el boton disabled*/
+                /*al precionar el boton +, count ira incrementando, de esta manera "stock y count seran verdaderos, activando asi el boton" */
                 stock && count
-                ? <button variant="contained" color="primary" onClick={() => onAdd(count)}>Agregar al Carrito</button>
+                ? <button variant="contained" color="primary" onClick={() => onAdd(count)}>Agregar al Carrito</button>/*al precionar el boton activa el evento click,llamando asi al metodo onAdd del componente ItemDetail, pasandole el valor de count, de esta manera cuando el componente itemDetail detecte el valor de count, como ya no sera cero, no llamara al componente ItemCount sino que dibujara un boton con la leyenda "terminar mi compra" asociado a un Link que redirecciona al precionarlo hacia el componente "Cart" */
 
                 : <button variant="contained" disabled>Agregar al Carrito</button>
             }
